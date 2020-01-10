d346c108300d759750810ade8e55b20f8ba9164e
jeremie on silex <jeremie.francois@gmail.com>
2020-01-10 17:36:16 +0100

    code cleanup + CSS rework

--- revealjs_keyinject_server.sh
+++ revealjs_keyinject_server.sh
@@ -1,27 +1,36 @@
 #!/bin/sh
 # Creates a minimalist web server on port 4444 (by default) with two buttons,
 # that in turn injects PageUp/Down keyboard events on the active (X) window.
-# This way, you can use your phone webbrowser to navigate in reveal.js.
+# This way, you can use your phone web browser to navigate in reveal.js.
 
 port=${1-4444}
-html='HTTP/1.1 200 OK
+contact=$(hostname -I | awk '{printf($1)}'; echo ":$port")
+
+html=$(cat << EOF
+HTTP/1.1 200 OK
 
 <html>
 	<style type="text/css">
 		body   { text-align: center; font-family: sans; background-color: black; color: white; }
-		a      { width:100%; color: white; text-decoration: none; font-size:300px; }
 		.Arrow { position: absolute; left: 0; top: 0; bottom: 0; }
-		.Left  { top: 4em; width: 50%; }
-		.Right { left: 50%; top: 4em; background-color:#444; }
-		.Bott  { position: fixed; bottom: 0; width: 50%; }
+		.Left  { top: 4em; width: 50%; background:linear-gradient(to right, #000, #666);}
+		.Right { left: 50%; top: 4em;  background:linear-gradient(to right, #888, #000); width:50%}
+		.Bott  { color: black; position: fixed; bottom: 0; width: 50%; font-size:200px; }
 	</style>
 	<body>
-		<center>Reveal.js remote control<br/>jeremie@obi2b.com</center>
-			<div class="Arrow Left">  <div class="Bott"> <a href="/?BKW"> &lArr; </a> </div> </div>
-			<div class="Arrow Right"> <div class="Bott"> <a href="/?FWD"> &rArr; </a> </div> </div>
+		<div> Reveal.js remote control <br/> jeremie@obi2b.com <br/> $contact </div>
+		<div class="Arrow Left"  onclick="move('BCK')"> <div class="Bott"> &lArr; </div> </div>
+		<div class="Arrow Right" onclick="move('FWD')"> <div class="Bott"> &rArr; </div> </div>
 	</body>
+	<script>
+		function move(param) {
+			console.log("move", param);
+			window.location = window.location.href.split(/[?#]/)[0] + "?" + param;
+		}
+	</script>
 </html>
-'
+EOF
+)
 
 echo "Reveal.js controlling web server is listening on $port..."
 while true; do
@@ -31,12 +40,12 @@ while true; do
 			timeout 0.5 netcat -l "$port" |
 				sed -n 's|GET /?\([A-Za-z0-1]*\).*|\1|p'
 	)
-
-	if [ "$action" = 'BKW' ]; then
-		echo "Back on $(date)"
+		
+	if [ "$action" = 'BCK' ]; then
+		echo "$action on $(date)"
 		xdotool key Page_Up
 	elif [ "$action" = 'FWD' ]; then
-		echo "Forward on $(date)"
+		echo "$action on $(date)"
 		xdotool key Page_Down
 	fi
 done
